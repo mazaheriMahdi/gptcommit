1098df16742d5d1943702a23278f406cddc231f4
Mahdi Mazaheri <mahdi.mazaheri8383@gmail.com>
2023-04-23 20:05:13 +0330

    add sha256sum to fm

--- Formula/gptcommit.rb
+++ Formula/gptcommit.rb
@@ -2,7 +2,7 @@ class Gptcommit < Formula
   desc "A tool for creating commit messages using GPT-3"
   homepage "https://github.com/mazaheriMahdi/gptcommit"
   url "https://github.com/mazaheriMahdi/gptcommit/releases/download/v1.0.0-alpha/gptcommit-macos-v1-beta-x64.tar.gz"
-  sha256 "f71ef679087176675d21ebcea92210b899b1cf358d3891bb7f40925f"
+  sha256 "85360909cf2943f401d11b90e8b282fb6a32c2936085997e511d6705bed0ebb2"
   version "1.0.0-alpha"
   def install
     bin.install "gptcommit"
